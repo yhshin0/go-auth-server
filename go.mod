d1cea66dd4f8491edacd22a8cb689c5f91b57cea
yhshin0 <asyh1047@naver.com>
2026-04-20 15:05:31 +0900

    feat: cors 설정 미들웨어 추가
    - 라이브러리 추가
    - 미들웨어 추가

--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require github.com/go-chi/chi/v5 v5.2.5
 require github.com/caarlos0/env/v11 v11.3.1
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/cors v1.2.2
--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 
 	"github.com/yhshin0/go-auth-server/internal/config"
 )
@@ -13,4 +14,9 @@ func Register(r *chi.Mux) {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Timeout(config.GetInstance().Server.HttpHandlerTimeout))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   config.GetInstance().CORS.AllowedOrigins,
+		AllowedMethods:   config.GetInstance().CORS.AllowedMethods,
+		AllowCredentials: config.GetInstance().CORS.AllowedCredentials,
+	}))
 }
